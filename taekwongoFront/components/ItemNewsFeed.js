651c7b31d831cadfa6ebffff5b6d4b4dc1e5c19b
felire <feliress13@gmail.com>
2018-09-05 01:57:37 -0300

    Cambio diseño al mostrar news

--- taekwongoFront/components/ItemNewsFeed.js
+++ taekwongoFront/components/ItemNewsFeed.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { Image } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Item } from 'native-base';
 
 
 export default class ItemNewsFeed extends Component {
@@ -24,21 +24,24 @@ export default class ItemNewsFeed extends Component {
         return (
             <Container>
                 <Content>
-                    <Card style={{flex: 0}}>
-                        <CardItem>
+                    <Card transparent style={{flex: 0, borderWidth:0}} >
+                        <CardItem bordered>
                             <Left>
-                                <Thumbnail source={{uri: 'https://image.ibb.co/mqPwkK/TAEKWONGO.png'}} />
                                 <Body>
-                                <Text>{item.title}</Text>
+                                <Text style={{fontWeight:'bold', fontSize:20}}>{item.title}</Text>
                                 </Body>
                             </Left>
                         </CardItem>
                         <CardItem>
                             <Body>
                             <Image source={{uri: item.picture_url}} style={{height: 200, width: '100%', flex: 1}}/>
-                            <Text>
-                                {item.body}
-                            </Text>
+                            </Body>
+                        </CardItem>
+                        <CardItem style={{marginTop:0}}>
+                            <Body>
+                                <Text style={{color:'#3e3e3e'}}>
+                                    {item.body}
+                                </Text>
                             </Body>
                         </CardItem>
                     </Card>
