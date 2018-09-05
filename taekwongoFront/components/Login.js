0c51b234f6606fe889f597f5ff541ea04cdae917
felire <feliress13@gmail.com>
2018-09-05 14:31:01 -0300

    Fix Pasaje de Login a entrenamientos

--- taekwongoFront/components/Login.js
+++ taekwongoFront/components/Login.js
@@ -129,8 +129,7 @@ export default class Login extends Component{
 
     onLogin(){
         this.setState({session_token: undefined}, function () {
-            this.callLoginApi(this.buildInfo());
-            this.openTrainingsView();
+            this.callLoginApi(this.buildInfo(), this.openTrainingsView);
         });
     }
 
@@ -156,8 +155,8 @@ export default class Login extends Component{
         }
     }
 
-    callLoginApi(info) {
-        fetch('http://192.168.0.43:3000/users/sessions', {
+    callLoginApi(info, onSuccess) {
+        fetch('http://taekwongo.herokuapp.com/users/sessions', {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
@@ -174,6 +173,7 @@ export default class Login extends Component{
                     AsyncStorage.setItem("access_token", response['access_token']);
                     AsyncStorage.setItem("renew_id", response['renew_id']);
                     this.setState({session_token: response['access_token']});
+                    onSuccess();
                 }
                 else {
                     console.log('No se comprendió el mensaje del servidor');
